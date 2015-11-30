fe7ceeb5ffcba4502e5e7145f84fbf90a40a6a4a
Sean McNamara <sean@coder.ie>
2015-11-29 18:37:44 -0800

    Switch to mySQL DB for development, also had to add a patch to actually be able to do that due to how annoying Windows is with Python dependencies.

--- manage.py
+++ manage.py
@@ -2,9 +2,18 @@
 import os
 import sys
 
+# [Start Patch] for Python mySQL on Windows causing me pain
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+# [End Patch]
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jlw.settings")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
