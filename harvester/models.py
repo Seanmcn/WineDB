323b2a67fdf1100dfc9accf3bebb6c4f89b666b2
Sean McNamara <sean@coder.ie>
2015-11-29 19:24:20 -0800

    Version 1.0 of Harvester model

---
+++ harvester/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class History(models.Model):
+    wine_count = models.IntegerField()
+    url = models.URLField()
+    date = models.DateTimeField('date harvested')
+
+
+class Wines(models.Model):
+    name = models.CharField(max_length=300)
+    location = models.CharField(max_length=300)
+    price = models.IntegerField()
+    rating = models.IntegerField()
+    description = models.TextField()
+    simple_type = models.CharField(max_length=50)
+    advanced_type = models.CharField(max_length=150)
+    mix = models.CharField(max_length=300)
+    date = models.DateTimeField('date published')
+    harvestedFrom = models.ForeignKey(History)
