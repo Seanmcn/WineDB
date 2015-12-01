b8a74aa2886febeaa5f0b7395783510a96e39974
Sean McNamara <sean@coder.ie>
2015-11-30 22:33:24 -0800

    Figured out some Django admin things and where to put scripts

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-
 from .models import Wines, History
 
-admin.site.register(Wines)
-admin.site.register(History)
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('url', 'wine_count', 'date')
+    list_filter = ['wine_count']
+    search_fields = ['url']
+
+
+class WinesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'grape', 'rating', 'date')
+    list_filter = ['color', 'grape', 'rating']
+    search_fields = ['name', 'color', 'grape']
+
+admin.site.register(Wines, WinesAdmin)
+admin.site.register(History, HistoryAdmin)
--- src/models.py
+++ src/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
 class History(models.Model):
     wine_count = models.IntegerField()
     url = models.URLField()
     date = models.DateTimeField('date harvested')
 
+    def __str__(self):
+        return self.url
+
 
 class Wines(models.Model):
     name = models.CharField(max_length=300)
+    color = models.CharField(max_length=50)
+    grape = models.CharField(max_length=150)
     location = models.CharField(max_length=300)
     price = models.IntegerField()
     rating = models.IntegerField()
     description = models.TextField()
-    simple_type = models.CharField(max_length=50)
-    advanced_type = models.CharField(max_length=150)
     mix = models.CharField(max_length=300)
     date = models.DateTimeField('date published')
     harvestedFrom = models.ForeignKey(History)
+
+    def __str__(self):
+        return self.name
