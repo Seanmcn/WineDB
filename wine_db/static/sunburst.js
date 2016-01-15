60ba8ba7987bf5a17a0bad6b3860cb41235e45fd
Sean McNamara <sean@coder.ie>
2016-01-14 19:04:17 -0800

    Fixing up comments and removing some unneeded commented out code

--- wine_db/static/sunburst.js
+++ wine_db/static/sunburst.js
@@ -19,7 +19,7 @@ var x = d3.scale.linear()
 var y = d3.scale.sqrt()
     .range([0, radius]);
 
-var color = d3.scale.category20c('#F64740', '#2274A5', '#E83F6F');
+var color = d3.scale.category20c();
 
 // Mapping of step names to colors.
 var colors = {
@@ -65,11 +65,12 @@ d3.json("data.json", function (error, root) {
     var data = svg.datum(root).selectAll("g")
         .data(partition.nodes(root));
 
+    // First group should contain all items with a depth < 2.
     var group1 = data.enter().append("g").attr("class", "g-inner").filter(function (d) {
         return d.depth < 2;
     });
 
-
+    // Append the item paths to group 1
     var path = group1.append("path")
         .attr("d", arc)
         .attr('id', function (d) {
@@ -87,14 +88,9 @@ d3.json("data.json", function (error, root) {
         .on("mouseleave", mouseleave)
         .each(stash);
 
-    //Create an SVG text element and append a textPath element
-    /*    var text = group1.append("text")
-     //.attr("x", function (d) {
-     //    return d.x;
-     //})   //Move the text from the start angle of the
-     //    .attr("transform", function (d) {
-     //return "rotate(" + computeTextRotation(d) + ")";
-     //})
+    // Playing with better rotation text
+/*
+     var text = group1.append("text")
      .attr("dy", function (d) {
      return (d.dy*120);
      }) //Move the text down
@@ -105,11 +101,14 @@ d3.json("data.json", function (error, root) {
      .style("text-anchor", "middle") //place the text halfway on the arc
      .attr("fill", "white") //place the text halfway on the arc
      .attr("startOffset", "20%")
-     //.attr('y', -20)
+
 
      .text(function (d) {
      return d.name;
-     });*/
+     });
+*/
+
+    // Description text on first two levels
     var text = group1.append("text")
         .attr("transform", function (d) {
             return "rotate(" + computeTextRotation(d) + ")";
@@ -117,9 +116,6 @@ d3.json("data.json", function (error, root) {
         .attr("x", function (d) {
             return y(d.y);
         })
-        //.attr("y", function (d) {
-        //    return x(d.x);
-        //})
         .attr("dx", "6") // margin
         .attr("dy", ".35em") // vertical-align
         .attr("fill", "white")
@@ -128,10 +124,12 @@ d3.json("data.json", function (error, root) {
             return d.name;
         });
 
+    // Group 2 will contain all the rest of the items
     var group2 = data.enter().append("g").filter(function (d) {
         return d.depth >= 2;
     }).attr("class", "g-outer");
 
+    // Append the item paths to group 2
     var path2 = group2.append("path")
         .attr("d", arc)
         .attr('id', function (d) {
@@ -149,6 +147,7 @@ d3.json("data.json", function (error, root) {
         .on("mouseleave", mouseleave)
         .each(stash);
 
+    // Setting up the hover text SVG
     svg.append("svg:text")
         .attr("x", "0")
         .attr("y", (maxHeight / 2) + (maxHeight / 12))
@@ -161,21 +160,24 @@ d3.json("data.json", function (error, root) {
         .attr("text-anchor", "middle")
         .attr("id", "sunburst_info");
 
-    //d3.selectAll("input").on("change", function change() {
-    //    var value = this.value === "count"
-    //        ? function () {
-    //        return 1;
-    //    }
-    //        : function (d) {
-    //        return d.size;
-    //    };
-    //
-    //    path
-    //        .data(partition.value(value).nodes)
-    //        .transition()
-    //        .duration(1000)
-    //        .attrTween("d", arcTweenData);
-    //});
+    // Changing the JSON data
+/*
+    d3.selectAll("input").on("change", function change() {
+        var value = this.value === "count"
+            ? function () {
+            return 1;
+        }
+            : function (d) {
+            return d.size;
+        };
+
+        path
+            .data(partition.value(value).nodes)
+            .transition()
+            .duration(1000)
+            .attrTween("d", arcTweenData);
+    });
+*/
 
     function click(d) {
         node = d;
@@ -184,6 +186,7 @@ d3.json("data.json", function (error, root) {
         console.log(d);
         console.groupEnd();
 
+        // Transition the path unless we are hitting level 3 and then we want to stop.
         if (d.depth === 3) {
             path.transition()
                 .duration(1000)
@@ -260,16 +263,12 @@ d3.json("data.json", function (error, root) {
             $('#info_title').html(d.parent.name + " - " + d.name);
             $('#info_content').html("");
             $('#normalWindow').show();
-            //document.getElementById("info_title").innerHTML = d.parent.name + " - " + d.name;
-            //document.getElementById("info_content").innerHTML = '';
         }
         else if (d.depth === 1) {
             $('#singleWine').hide();
             $('#info_title').html(d.name);
             $('#info_content').html("");
             $('#normalWindow').show();
-            //document.getElementById("info_title").innerHTML = d.name;
-            //document.getElementById("info_content").innerHTML = '';
         }
         else {
             $('.level-text-two').show();
@@ -277,8 +276,6 @@ d3.json("data.json", function (error, root) {
             $('#info_title').html(d.name);
             $('#info_content').html("Use the chart to browse through the wines.");
             $('#normalWindow').show();
-            //document.getElementById("info_title").innerHTML = d.name;
-            //document.getElementById("info_content").innerHTML = 'Use the chart to browse through the wines.';
         }
     }
 
@@ -287,9 +284,10 @@ d3.json("data.json", function (error, root) {
         d3.selectAll("path")
             .style("opacity", 0.5);
 
+        // Un-fade the path we've chosen
         d3.select("path").style("opacity", 1);
 
-        // Then highlight only those that are an ancestor of the current segment.
+        // Then un-fade those that are an ancestor of the current segment.
         var sequenceArray = getAncestors(d);
         d3.selectAll("path")
             .filter(function (node) {
@@ -297,60 +295,18 @@ d3.json("data.json", function (error, root) {
             })
             .style("opacity", 1);
 
+        // Hover text
         $('#sunburst_info').html(d.name);
     }
 
     function mouseleave(d) {
         d3.selectAll("path").style("opacity", 1);
-        //$('#sunburst_info').empty();
     }
-
-    /*
-     function locationHashChanged() {
-     var hash = location.hash.replace(/#/i, '');
-     //var d = d3.select($('#sunburst_' + hash));
-     //var d = d3.select('M1.1257428731061414e-14,-183.84776310850236A183.84776310850236,183.84776310850236 0 0,1 109.04607535919388,148.01673367817915L77.10721934826528,104.6636361129237A130,130 0 0,0 7.960204194457796e-15,-130Z');
-     //var d = d3.selectAll('path#sunburst_' + hash);
-     var paths = d3.select("svg").selectAll("path")[0];
-
-     //console.log(paths);
-     //console.log(hash);
-     for (var i = 0; i < paths.length; i++) {
-     if(paths[i]['id'] == "sunburst_"+hash) {
-     console.log(paths[i]);
-     }
-
-     //console.log("MISS:", paths[i].id);
-     //break;
-
-     }
-     //console.log(d);
-
-     //node = d;
-     //var path = svg.datum(root).selectAll("path");
-     //arcTweenZoom(d);
-
-
-     //var path = svg;
-     //.data(partition.nodes)
-     //.enter().append("path")
-     //console.log(path);
-
-     //path.transition()
-     //    .duration(1000)
-     //    .attrTween("d", arcTweenZoom(d));
-
-     }
-
-     window.onhashchange = locationHashChanged();
-     */
-
 });
 
-
 d3.select(self.frameElement).style("height", height + "px");
-// Given a node in a partition layout, return an array of all of its ancestor
-// nodes, highest first, but excluding the root.
+
+// Given a node in a partition layout, return an array of all of its ancestor nodes, highest first, but excluding the root.
 function getAncestors(node) {
     var path = [];
     var current = node;
@@ -360,6 +316,7 @@ function getAncestors(node) {
     }
     return path;
 }
+
 // Setup for switching data: stash the old values for transition.
 function stash(d) {
     d.x0 = d.x;
@@ -378,8 +335,7 @@ function arcTweenData(a, i) {
     }
 
     if (i == 0) {
-        // If we are on the first arc, adjust the x domain to match the root node
-        // at the current zoom level. (We only need to do this once.)
+        // If we are on the first arc, adjust the x domain to match the root node at the current zoom level. (We only need to do this once.)
         var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
         return function (t) {
             x.domain(xd(t));
@@ -409,9 +365,5 @@ function arcTweenZoom(d) {
 }
 
 function computeTextRotation(d) {
-    //console.group("computeTextRotation");
-    //console.log("x", x(d.x + d.dx / 2));
-    //console.log((x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180);
-    //console.groupEnd();
     return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
 }
